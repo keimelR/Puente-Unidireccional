4c95c4b3f8df7c98a2331271145e8549bc02bb14
keimelR <keimelrodriguez12@gmail.com>
2025-07-13 23:07:15 +0200

    Representacion logica del vehiculo

---
+++ model/Vehicle.py
@@ -0,0 +1,26 @@
+from model.Direccion import Direccion
+
+class Vehicle:
+    """
+    Representacion logica de las caracteristicas de los vehiculos que cruzan el puente
+    """
+    def __init__(
+        self,
+        id: str,
+        velocidad: float = 0.0,
+        tiempo_retraso: float = 0.0,
+        direccion: Direccion = Direccion.NONE
+    ):
+        """
+        Constructor de la clase
+        
+        Args:
+            id (str): Identificador unico del vehiculo
+            velocidad (float): Velocidad del vehiculo en el puente 
+            tiempo_retraso (float): Tiempo promedio despues de cruzar del vehiculo antes de intentar otro cruce
+            direccion (Direccion): Direccion de rumbo inicial del vehiculo
+        """
+        self.id = id
+        self.velocidad = velocidad
+        self.tiempo_retraso = tiempo_retraso
+        self.direccion = direccion 
